d46afe703c8fd6991fdfbf170cfad465a5627774
Hemanathan H P <hphemanathan@gmail.com>
2025-07-30 12:59:41 +0530

    Add files via upload

---
+++ src/components/Experience/Experience.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+// import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import { useThree, extend, useFrame } from "@react-three/fiber";
+extend({ OrbitControls });
+
+function Experience() {
+
+      const { camera, gl } = useThree();
+  // console.log(three);
+
+
+
+  useFrame((state, delta) =>
+    {
+      cubeRef.current.rotation.y += delta
+      // groupRef.current.rotation.y += delta;
+
+    })
+    const cubeRef = useRef();
+    const groupRef = useRef();
+
+  return (
+    <>
+      <orbitControls args={[camera, gl.domElement]} />
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color='orange' />
+        </mesh>
+
+        <mesh
+          ref={cubeRef}
+          rotation-y={Math.PI * 0.25}
+          position-x={2}
+          scale={1.5}>
+          <boxGeometry />
+          <meshBasicMaterial color='mediumpurple' />
+        </mesh>
+      </group>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color='greenyellow' />
+      </mesh>
+    </>
+  );
+}
+
+export default Experience;
---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import './style.css'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from "@react-three/fiber";
+import { MeshNormalMaterial } from 'three';
+import Experience from './components/Experience/Experience';
+
+const root = ReactDOM.createRoot(document.querySelector('#root'))
+
+root.render(
+  
+        <Canvas>
+           <Experience/>
+        </Canvas>
+  
+)
